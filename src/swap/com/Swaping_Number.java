f995aaa65e42f3412fc8928ea5fc595a025e2e97
Sovish1303 <sovishnuchringth1303@gmail.com>
2024-03-26 12:52:52 +0530

    this is first push

---
+++ src/swap/com/Swaping_Number.java
@@ -0,0 +1,13 @@
+package swap.com;
+
+public class Swaping_Number {
+	public static void main (String[] args)
+	{
+		int A=7;
+		int b=9;
+		A=(A*b)/(b=A);
+		System.out.println(A+" "+b);
+
+	}
+}
+
